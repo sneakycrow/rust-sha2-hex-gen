f40f2f206b15114f209412781643bb36e5a5830e
Zachary Sohovich <zachsohovich@thebriga.de>
2019-08-29 11:22:30 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use sha2::{Sha256, Digest};
+use std::io;
+
+fn main() {
+  // Grab the Secret from the user
+  let mut secret_input = String::new();
+  // Read line the input they created and store it in the secret_type var
+  io::stdin().read_line(&mut secret_input).ok().expect("Couldn't read line");
+  // Create a new hasher
+  let mut hasher = Sha256::new();
+  // Write the input to the hasher
+  hasher.input(secret_input);
+  // Read the hash digest and consume the hasher
+  let result = hasher.result();
+  // Send the result back to the user
+  println!("{:?}", hex::encode(result));
+}
